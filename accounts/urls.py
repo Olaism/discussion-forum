2fe2d5cc37895e82435bbec1d87e99e98e98c3a4
olaism <olaism01@gmail.com>
2023-03-20 12:17:49 -0700

    implemented logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 from .views import signup
 
 urlpatterns = [
     path('signup/', signup, name='signup'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
--- conf/settings.py
+++ conf/settings.py
@@ -106,3 +106,5 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGOUT_REDIRECT_URL = 'login'
