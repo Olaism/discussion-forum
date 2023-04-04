650c3b4c22c9e43b83275ba4919fffdaa45bdd99
olaism <olaism01@gmail.com>
2023-04-04 09:02:56 -0700

    template rededigned

--- boards/models.py
+++ boards/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils.html import mark_safe
 from django.utils.text import Truncator
+
 from markdown import markdown
 
 User = get_user_model()
