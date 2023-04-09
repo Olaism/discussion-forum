3ca75d1aaeb0e5ca8aac541bf6b33d66575bb1e1
olaism <olaism01@gmail.com>
2023-04-09 05:19:44 -0700

    add a share_post functionalities (url, view and template)

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
+from django.conf import settings
+from django.core.mail import send_mail
 from django.shortcuts import get_object_or_404, render
-from django.views.generic import DetailView, ListView
+from django.views.generic import DetailView, FormView, ListView
 
+from .forms import EmailPostForm
 from .models import Post
 
 
@@ -33,3 +36,27 @@ class PostDetailView(DetailView):
             publish__month=self.kwargs.get('month'),
             publish__day=self.kwargs.get('day')
         )
+
+
+def post_share(request, pk):
+    # reference post by id
+    post = get_object_or_404(Post, pk=pk)
+    sent = False
+
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # form fields pass validation
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = '{} ({}) recommends you reading "{}"'.format(cd['name'], cd['email'], post.title)
+            message = 'Read "{}" at {}\n\n{}\'s comments: {}'.format(
+                post.title, post_url, cd['name'], cd['comment']
+            )
+            send_mail(subject, message, settings.EMAIL_HOST_USER, [cd['to'], ])
+            sent = True
+
+    else:
+        form = EmailPostForm()
+
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
--- conf/settings.py
+++ conf/settings.py
@@ -108,4 +108,10 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "login"
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_PORT = 587
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS = True
