ed84f0ac6e4e45f8fd099bb0b214dc063f2c08d2
olaism <olaism01@gmail.com>
2023-04-12 10:14:34 -0700

    add markdown to blog template_tag"

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,4 +1,7 @@
 from django import template
+from django.utils.safestring import mark_safe
+
+import markdown
 
 from ..models import Post
 
@@ -15,3 +18,8 @@ def total_posts():
 def show_latest_posts(count=5):
     latest_posts = Post.published.order_by('-publish')[:count]
     return {'latest_posts': latest_posts}
+
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
